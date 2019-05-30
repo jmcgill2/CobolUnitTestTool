334588f0923fecfdd6367910cce0764eb75eef71
jamesmcgill <jmcgill2a@gmail.com>
2019-05-30 13:42:00 -0500

    Initial commit of the Environmental Division.

---
+++ src/main/groovy/org/jmcgill2/cobol/data/EnvironmentalDivision.groovy
@@ -0,0 +1,13 @@
+package org.jmcgill2.cobol.data
+
+import groovy.util.logging.Slf4j
+
+/**
+ * Contains Environmental Division Content from the Cobol Program.
+ * This includes the Configuration Section and the Input-Output Section.
+ */
+@Slf4j
+class EnvironmentalDivision {
+
+    ConfigurationSection configurationSection
+}
