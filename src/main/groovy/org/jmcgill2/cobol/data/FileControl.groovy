10ad6a5b1f70ba6d9208f83a2e37b37411ae98f1
jamesmcgill <jmcgill2a@gmail.com>
2019-05-30 14:08:10 -0500

    Initial commit of the FileControl.

--- src/main/groovy/org/jmcgill2/cobol/data/FileControl.groovy
+++ src/main/groovy/org/jmcgill2/cobol/data/FileControl.groovy
@@ -1,6 +1,28 @@
 package org.jmcgill2.cobol.data
 
+import groovy.util.logging.Slf4j
+
+/**
+ * Contains the File Control information for the Input-Output Section of the Environment Division.
+ */
+@Slf4j
 class FileControl {
 
     ArrayList<CobolLine> fileControlLines = []
+
+    FileControl(){
+
+    }
+
+    FileControl(ArrayList<CobolLine> fileControlLines){
+        this.fileControlLines = fileControlLines
+    }
+
+    String toString() {
+        String str = ""
+        fileControlLines.each{CobolLine cl ->
+            str += cl.lineWithoutLineNumber + "\n"
+            return str.toString()
+        }
+    }
 }
