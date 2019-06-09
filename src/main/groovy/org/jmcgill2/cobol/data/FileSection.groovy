6b37f483362b2dcac560a1a96411a3b89bbf0e27
jamesmcgill <jmcgill2a@gmail.com>
2019-06-08 22:35:09 -0500

    Added constructor to the FileSection class.

--- src/main/groovy/org/jmcgill2/cobol/data/FileSection.groovy
+++ src/main/groovy/org/jmcgill2/cobol/data/FileSection.groovy
@@ -1,8 +1,17 @@
 package org.jmcgill2.cobol.data
 
+/**
+ * Contains the File Section information from the Data Division of the Cobol Program.
+ */
 class FileSection {
 
     ArrayList<CobolLine> fileSectionLines = []
 
+    FileSection(){
 
+    }
+
+    FileSection(ArrayList<CobolLine> fileSectionLines){
+        this.fileSectionLines = fileSectionLines
+    }
 }
