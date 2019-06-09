08d210019306d2847803ceb574ab942eb09f9040
jamesmcgill <jmcgill2a@gmail.com>
2019-06-08 22:33:48 -0500

    Updated DataDivision class.

--- src/main/groovy/org/jmcgill2/cobol/data/DataDivision.groovy
+++ src/main/groovy/org/jmcgill2/cobol/data/DataDivision.groovy
@@ -1,8 +1,38 @@
 package org.jmcgill2.cobol.data
 
+import org.jmcgill2.cobol.utils.CobolUtils
+
+/**
+ * Contains the contents of the Cobol program Data Division.  This includes the File Section, Working Storage Section and
+ * Linkage Section.
+ */
 class DataDivision {
 
+    FileSection fileSection
+
+    WorkingStorageSection workingStorageSection
+
+    LinkageSection linkageSection
+
     ArrayList<CobolLine> dataDivisionLines = []
+
+    CobolUtils cobolUtils = new CobolUtils()
+
+    DataDivision(){
+
+    }
+
+    DataDivision(ArrayList<CobolLine> dataDivisionLines){
+        this.dataDivisionLines = dataDivisionLines
+    }
+
+    DataDivision(FileSection fileSection, WorkingStorageSection workingStorageSection, LinkageSection linkageSection,
+                ArrayList<CobolLine> dataDivisionLines){
+        this.fileSection = fileSection
+        this.workingStorageSection = workingStorageSection
+        this.linkageSection = linkageSection
+        this.dataDivisionLines = dataDivisionLines
+    }
 
 
 }
